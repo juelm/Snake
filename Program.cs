50959cb19683b931beb22747654ff09628c11c5a
Matthew Juel <matthewjuel@g5pb13.mlearning.nh>
2019-10-10 13:40:34 -0700

    Initial Snake Commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,58 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = 1;
+            int y = 1;
+            Snake snake = new Snake(1,1,50);
+            snake.draw();
+            ConsoleKey action = ConsoleKey.F;
+
+            while (action != ConsoleKey.Q)
+            {
+                //quit = Console.ReadKey().Key;
+                //int oldX = x;
+                //int oldY = y;
+
+                //switch (quit)
+                //{
+                //    case ConsoleKey.UpArrow:
+                //        y = y - 1;
+                //        if (pacMan.Equals(">") || pacMan.Equals("<") || pacMan.Equals("-") || pacMan.Equals("\u0C79") || pacMan.Equals("\u02C4")) pacMan = "\u02C5";
+                //        else if (pacMan.Equals(">") || pacMan.Equals("<") || pacMan.Equals("-") || pacMan.Equals("\u02C5") || pacMan.Equals("\u02C4")) pacMan = "\u0C79";
+                //        break;
+                //    case ConsoleKey.DownArrow:
+                //        y = y + 1;
+                //        if (pacMan.Equals(">") || pacMan.Equals("<") || pacMan.Equals("-") || pacMan.Equals("\u0C79") || pacMan.Equals("\u02C5")) pacMan = "\u02C4";
+                //        else if (pacMan.Equals(">") || pacMan.Equals("<") || pacMan.Equals("-") || pacMan.Equals("\u02C5") || pacMan.Equals("\u02C4")) pacMan = "\u0C79";
+                //        break;
+                //    case ConsoleKey.LeftArrow:
+                //        x = x - 1;
+                //        if (pacMan.Equals("\u02C4") || pacMan.Equals("<") || pacMan.Equals("-") || pacMan.Equals("\u0C79") || pacMan.Equals("\u02C5")) pacMan = ">";
+                //        else if (pacMan.Equals(">") || pacMan.Equals("<") || pacMan.Equals("\u0C79") || pacMan.Equals("\u02C5") || pacMan.Equals("\u02C4")) pacMan = "-";
+                //        break;
+                //    case ConsoleKey.RightArrow:
+                //        x = x + 1;
+                //        if (pacMan.Equals(">") || pacMan.Equals("\u02C4") || pacMan.Equals("-") || pacMan.Equals("\u0C79") || pacMan.Equals("\u02C5")) pacMan = "<";
+                //        else if (pacMan.Equals(">") || pacMan.Equals("<") || pacMan.Equals("\u0C79") || pacMan.Equals("\u02C5") || pacMan.Equals("\u02C4")) pacMan = "-";
+                //        break;
+                //}
+
+                ////if (pacMan.Equals("<")) pacMan = "-";
+                ////else pacMan = "<";
+
+                //WriteAt(pacMan, x, y);
+                //WriteAt(" ", oldX, oldY);
+
+                while (action != ConsoleKey.Q)
+                {
+                    snake.draw();
+
+                    action = Console.ReadKey().Key;
+
+                    if (action == ConsoleKey.UpArrow) snake.slither();
+
+                }
+            }
         }
     }
 }
---
+++ Snake.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Timers;
+namespace Snake
+{
+    public class Snake
+    {
+        private int positionX;
+        private int positionY;
+        private int priorY;
+        private char icon = '<';
+        private System.Timers.Timer timer;
+
+        public int PositionY
+        {
+
+            get
+            {
+                return positionY;
+            }
+
+            set
+            {
+                positionY = value;
+            }
+
+        }
+
+        public Snake(int x, int y, int ms)
+        {
+            positionX = x;
+            positionY = y;
+            timer = new System.Timers.Timer(ms);
+            timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            timer.Enabled = true;
+        }
+
+        public void flap()
+        {
+            priorY = PositionY;
+
+            PositionY -= 3;
+
+        }
+
+        public void slither()
+        {
+            priorY = positionY;
+            PositionY += 1;
+
+            draw();
+        }
+
+        public void draw()
+        {
+            Console.SetCursorPosition(positionX, positionY);
+            Console.Write(icon);
+
+            Console.SetCursorPosition(positionX, priorY);
+            Console.Write(" ");
+        }
+
+        public void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            slither();
+        }
+    }
+}

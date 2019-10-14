da2d884798727511c71db4c63e91c91f13c249e7
Matthew Juel <matthewjuel@g5pb05.mlearning.nh>
2019-10-14 07:59:07 -0700

    merging Sackariya Bot

---
+++ snake/ComputerSnake.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Snake
+{
+    public class ComputerSnake : Snake
+    {
+        public ComputerSnake(int x, int y)
+        {
+            PositionX = x;
+            PositionY = y;
+        }
+    }
+}
